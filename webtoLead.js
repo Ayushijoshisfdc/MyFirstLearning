14127ac81e4ab2b1e55cdcae23460528408c9f6d
Ayushi Joshi <ayushijoshi19@gmail.com>
2023-09-05 18:16:46 +0530

    first commit

---
+++ webtoLead.js
@@ -0,0 +1,16 @@
+function beforesubmit (){
+   
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate", inputdate.value);
+
+    let formatteddate = new Date (inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatteddate;
+    
+    
+
+  
+
+}   
+
+
